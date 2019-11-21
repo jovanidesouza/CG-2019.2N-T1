2cd9078408763ef5ea6ced0293d4ba9d05f67843
Jovani de Souza <jovanidesouza@hotmail.com>
2019-11-21 13:01:00 -0300

    Add Voltorb - Simple pokémon

---
+++ js/desenho.js
@@ -0,0 +1,70 @@
+//alert('hello!');
+var scene = new THREE.Scene();
+
+		
+			
+			var camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 0.1, 1000 );
+			camera.position.set( 0, 0, 15 );
+			//camera.position.z = 15;
+
+			var renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
+			renderer.setClearColor (0x20bfff, 1);
+			
+			var fundo = new THREE.TextureLoader().load( 'textures/floor.jpg' );
+			scene.background = fundo;
+			//Scene, camera , renderer //
+					
+	
+			
+			///ESFERA
+			var geometry = new THREE.SphereGeometry( 0.5, 32, 32 );
+			var texture = new THREE.TextureLoader().load( 'textures/bola.PNG' );
+			// immediately use the texture for material creation
+			var material = new THREE.MeshBasicMaterial( { map: texture } );
+			//var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+			var sphere = new THREE.Mesh( geometry, material );
+			sphere.translateZ(12)
+			sphere.translateY(-1);
+			sphere.rotateY(-1.5);
+			sphere.rotateX(0.1);
+			sphere.rotateZ(0.6);
+			scene.add( sphere );
+			
+			////////////////////
+			
+			
+			// movement - please calibrate these values
+			
+			var xSpeed = 0.05;
+			var ySpeed = 0.05;
+			
+			document.addEventListener("keydown", onDocumentKeyDown, false);
+			function onDocumentKeyDown(event) {
+				var keyCode = event.which;
+				if (keyCode == 39) {
+					sphere.position.x += xSpeed;
+					//sphere.translateX(xSpeed)
+				} else if(keyCode == 37) {
+					sphere.position.x -= xSpeed;
+				} else if(keyCode == 38) {
+					sphere.position.y += ySpeed;
+				} else if(keyCode == 40){
+					sphere.position.y -= ySpeed;
+				}
+			};
+			
+			
+
+					
+			var animate = function () {
+				requestAnimationFrame( animate );
+	
+				//sphere.rotation.x -= 0.05;
+
+				
+				renderer.render( scene, camera );
+			};
+
+			animate();
